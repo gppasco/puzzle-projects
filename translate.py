a2a3cc3ab376c343c7cdf26705f214b3c7bfe61b
Paolo Pasco <gpaolopasco@gmail.com>
2020-12-16 00:29:00 -0800

    Add files via upload
    using google translate's API to make a puzzle where the clues are translated into a number different languages, then back to english

---
+++ translate.py
@@ -0,0 +1,162 @@
+
+# ********** LET'S TRANSLATE SOME STUFF *****************
+# SOURCE: https://github.com/lushan88a/google_trans_new
+
+from google_trans_new import google_translator
+import puz
+import re
+
+# LANGUAGES
+
+LANGUAGES = {
+    'af': 'afrikaans',
+    'sq': 'albanian',
+    'am': 'amharic',
+    'ar': 'arabic',
+    'hy': 'armenian',
+    'az': 'azerbaijani',
+    'eu': 'basque',
+    'be': 'belarusian',
+    'bn': 'bengali',
+    'bs': 'bosnian',
+    'bg': 'bulgarian',
+    'ca': 'catalan',
+    'ceb': 'cebuano',
+    'ny': 'chichewa',
+    'zh-cn': 'chinese (simplified)',
+    'zh-tw': 'chinese (traditional)',
+    'co': 'corsican',
+    'hr': 'croatian',
+    'cs': 'czech',
+    'da': 'danish',
+    'nl': 'dutch',
+    'en': 'english',
+    'eo': 'esperanto',
+    'et': 'estonian',
+    'tl': 'filipino',
+    'fi': 'finnish',
+    'fr': 'french',
+    'fy': 'frisian',
+    'gl': 'galician',
+    'ka': 'georgian',
+    'de': 'german',
+    'el': 'greek',
+    'gu': 'gujarati',
+    'ht': 'haitian creole',
+    'ha': 'hausa',
+    'haw': 'hawaiian',
+    'iw': 'hebrew',
+    'he': 'hebrew',
+    'hi': 'hindi',
+    'hmn': 'hmong',
+    'hu': 'hungarian',
+    'is': 'icelandic',
+    'ig': 'igbo',
+    'id': 'indonesian',
+    'ga': 'irish',
+    'it': 'italian',
+    'ja': 'japanese',
+    'jw': 'javanese',
+    'kn': 'kannada',
+    'kk': 'kazakh',
+    'km': 'khmer',
+    'ko': 'korean',
+    'ku': 'kurdish (kurmanji)',
+    'ky': 'kyrgyz',
+    'lo': 'lao',
+    'la': 'latin',
+    'lv': 'latvian',
+    'lt': 'lithuanian',
+    'lb': 'luxembourgish',
+    'mk': 'macedonian',
+    'mg': 'malagasy',
+    'ms': 'malay',
+    'ml': 'malayalam',
+    'mt': 'maltese',
+    'mi': 'maori',
+    'mr': 'marathi',
+    'mn': 'mongolian',
+    'my': 'myanmar (burmese)',
+    'ne': 'nepali',
+    'no': 'norwegian',
+    'or': 'odia',
+    'ps': 'pashto',
+    'fa': 'persian',
+    'pl': 'polish',
+    'pt': 'portuguese',
+    'pa': 'punjabi',
+    'ro': 'romanian',
+    'ru': 'russian',
+    'sm': 'samoan',
+    'gd': 'scots gaelic',
+    'sr': 'serbian',
+    'st': 'sesotho',
+    'sn': 'shona',
+    'sd': 'sindhi',
+    'si': 'sinhala',
+    'sk': 'slovak',
+    'sl': 'slovenian',
+    'so': 'somali',
+    'es': 'spanish',
+    'su': 'sundanese',
+    'sw': 'swahili',
+    'sv': 'swedish',
+    'tg': 'tajik',
+    'ta': 'tamil',
+    'te': 'telugu',
+    'th': 'thai',
+    'tr': 'turkish',
+    'uk': 'ukrainian',
+    'ur': 'urdu',
+    'ug': 'uyghur',
+    'uz': 'uzbek',
+    'vi': 'vietnamese',
+    'cy': 'welsh',
+    'xh': 'xhosa',
+    'yi': 'yiddish',
+    'yo': 'yoruba',
+    'zu': 'zulu'}
+
+
+# READING PUZ FILE
+
+p = puz.read("puztranslate/nd201216.puz")
+
+old_clues = p.clues
+new_clues = []
+
+# TRANSLATING THAT BAD BOY
+from multiprocessing.dummy import Pool as ThreadPool
+import time
+
+pool = ThreadPool(8) # Threads
+
+def request(text):
+    lang = "fr"
+    t = google_translator(timeout=5)
+    # TRANSLATES TO RANDOM LANGUAGES A RANDOM NUMBER OF TIMES
+    num_translations = 5
+    translate1 = t.translate(text.strip(), lang)
+    translate2 = t.translate(translate1, "en")
+    translate3 = t.translate(translate2, lang)
+    translateFINAL = t.translate(translate3, "en")
+    translateFINAL = re.sub("\u201c", "\"", translateFINAL)
+
+    return translateFINAL
+
+if __name__ == "__main__" :
+    time1 = time.time()
+    texts = p.clues
+    print("Translating...")
+    results = pool.map(request, texts)
+    pool.close()
+    pool.join()
+
+    time2 = time.time()
+    print("Translating %s sentences, a total of %s s"%(len(texts),time2 - time1))
+
+
+
+p.clues = results
+
+p.save('translated.puz')
